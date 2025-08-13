3f91a21cb672fccc44dd51ad2e5b24e0babacf85
MrFlava <thatelitemaili33t@gmail.com>
2025-08-13 11:30:04 +0300

    Provide Country model

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+
+from .db import Base
+
+class Country(Base):
+    __tablename__ = 'countries'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    population = Column(Integer, nullable=False)
+    region = Column(String, nullable=False)
