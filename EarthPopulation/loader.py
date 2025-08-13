b5357ed06d181510182a2d841c976be163d8919c
MrFlava <thatelitemaili33t@gmail.com>
2025-08-13 12:59:01 +0300

    Parse data in data loader

--- EarthPopulation/loader.py
+++ EarthPopulation/loader.py
@@ -14,7 +14,17 @@ class DataLoader:
         table = soup.find("table", {"class": "wikitable"})
         rows = table.find_all("tr")[1:]
 
-        print(rows)
+        for row in rows:
+            cols = row.find_all("td")
+            if len(cols) < 5:
+                continue
+
+            country_name = cols[0].get_text(strip=True)
+            pop_2023 = cols[2].get_text(strip=True).replace(",", "")
+            region = cols[4].get_text(strip=True)
+
+            if pop_2023.isdigit():
+                print(country_name, pop_2023, region)
 
 if __name__ == "__main__":
     DataLoader().get_data()
